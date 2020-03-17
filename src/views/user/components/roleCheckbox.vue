b9d2f63971e3e1af636b71b546bf3692a22179d0
sunpiaoliang <sunpeng@sunpiaoliang.cn>
2020-03-17 22:49:03 +0800

    用户页面布局设计

---
+++ src/views/user/components/roleCheckbox.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <el-card class="box-card" style="width:100%">
+    <div slot="header" class="clearfix">
+      <span>角色</span>
+      <el-button style="float: right; padding: 3px 0" type="text">
+        <i class="el-icon-circle-plus-outline" />
+      </el-button>
+    </div>
+    <div>
+
+      <el-checkbox-group v-model="checkedList" @change="handleCheckedChange">
+        <el-checkbox v-for="checked in roleList" :key="checked" :label="checked">{{ checked }}</el-checkbox>
+      </el-checkbox-group>
+    </div>
+  </el-card>
+
+</template>
+
+<script>
+export default {
+  name: 'RoleCheckbox',
+  data() {
+    return {
+      roleList: ['第一个', '第二个'],
+      checkedList: ['第二个']
+    }
+  },
+
+  methods: {
+    handleCheckedChange(value) {
+      console.log(value)
+    }
+  }
+}
+
+</script>
+
+<style lang="sass">
+</style>
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,58 +1,87 @@
 <template>
   <div class="app-container">
-    <el-table
-      :key="tableKey"
-      v-loading="listLoading"
-      :data="list"
-      border
-      fit
-      highlight-current-row
-      style="width: 75%;"
-    >
+    <el-row :gutter="10">
+      <el-col :md="4">
+        <el-card class="box-card" style="width:100%;height:1000px">
+          <div slot="header" class="clearfix">
+            <span>集团</span>
+            <el-button style="float: right; padding: 3px 0" type="text">
+              <i class="el-icon-circle-plus-outline" />
+            </el-button>
+          </div>
+          <div>
+            <DeptTree />
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :lg="20">
+        <el-card class="box-card" style="width:100%">
+          <div slot="header" class="clearfix">
+            <span>用户</span>
+            <el-button style="float: right; padding: 3px 0" type="text">
+              <i class="el-icon-circle-plus-outline" />
+            </el-button>
+          </div>
+          <div>
 
-      <el-table-column label="用户名" min-width="12%" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.loginName }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="年龄" min-width="8%" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.age }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="电话" min-width="15%" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.phone }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="激活状态" min-width="8%" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.isActived | parseIsActived }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="添加时间" min-width="15%" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.inputDate }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="添加人" min-width="12%" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.inputName }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column :render-header="renderHeader" align="center" min-width="20%" class-name="small-padding fixed-width">
-        <template slot-scope="{row,$index}">
-          <el-button type="primary" size="mini">
-            编辑
-          </el-button>
-          <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="handleDelete(row,$index)">
-            注销
-          </el-button>
-        </template>
-      </el-table-column>
+            <el-table
+              :key="tableKey"
+              v-loading="listLoading"
+              :data="list"
+              border
+              fit
+              highlight-current-row
+              style="width: 100%;"
+            >
 
-    </el-table>
-    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+              <el-table-column label="用户名" min-width="12%" align="center">
+                <template slot-scope="{row}">
+                  <span>{{ row.loginName }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="年龄" min-width="8%" align="center">
+                <template slot-scope="{row}">
+                  <span>{{ row.age }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="电话" min-width="15%" align="center">
+                <template slot-scope="{row}">
+                  <span>{{ row.phone }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="激活状态" min-width="8%" align="center">
+                <template slot-scope="{row}">
+                  <span>{{ row.isActived | parseIsActived }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="添加时间" min-width="15%" align="center">
+                <template slot-scope="{row}">
+                  <span>{{ row.inputDate }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="添加人" min-width="12%" align="center">
+                <template slot-scope="{row}">
+                  <span>{{ row.inputName }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column :render-header="renderHeader" align="center" min-width="20%" class-name="small-padding fixed-width">
+                <template slot-scope="{row,$index}">
+                  <el-button type="primary" size="mini">
+                    编辑
+                  </el-button>
+                  <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="handleDelete(row,$index)">
+                    注销
+                  </el-button>
+                </template>
+              </el-table-column>
+
+            </el-table>
+            <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+          </div>
+        </el-card>
+        <RoleCheckbox style="margin-top:20px" />
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -60,10 +89,12 @@
 
 import Pagination from '@/components/Pagination'
 import { getUserList } from '@/api/user'
+import RoleCheckbox from './components/roleCheckbox'
+import DeptTree from './components/deptTree'
 
 export default {
   name: 'User',
-  components: { Pagination },
+  components: { Pagination, RoleCheckbox, DeptTree },
   filters: {
     parseIsActived(isActived) {
       if (isActived) {
@@ -110,16 +141,13 @@ export default {
       getUserList(this.listQuery).then(response => {
         this.list = response.result.list
         this.total = response.result.total
-        setTimeout(() => {
-          this.listLoading = false
-        }, 1.5 * 1000)
+        this.listLoading = false
       })
     },
     renderHeader() {
       return (
         <div>
           <span>操作</span>
-          <a style='margin-left:10px'><i class='el-icon-circle-plus-outline' /></a>
         </div>
       )
     },
